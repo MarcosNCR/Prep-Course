812abe3bb86f9b762566ed3b43ab1640a9dd86db
MarcosNCR <marcossebastiangonzalezaredes@gmail.com>
2023-01-18 22:41:16 -0300

    commit nuevo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = Hola;
 const nuevoNum = null;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = 5;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
